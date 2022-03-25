b82c0a7a0bc980c7c325e82d2d15f5618f50a647
rishabhreng <rishabhreng@gmail.com>
2022-03-25 08:33:48 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022txfor",
+        "defaultValue":"2022txirv",
         "required":"true",
         "disabled":"true"
       },
